6e0900d2965c3b751397515f94d96268acf8f11c
Yasein Burqan <mr.yasein@gmail.com>
2022-07-17 02:12:40 +0300

    Contact-Manager

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,17 +4,11 @@ import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   console.log(props);
 
-  const deleteConactHandler = (id) => {
+  const deleteContactHandler = (id) => {
     props.getContactId(id);
   };
   const renderContactList = props.contacts.map((contact) => {
-    return (
-      <ContactCard
-        contact={contact}
-        clickHander={deleteConactHandler}
-        key={contact.id}
-      />
-    );
+    return <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id} />;
   });
   return <div className="ui celled list">{renderContactList}</div>;
 };
